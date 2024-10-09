a63b615f43afd337fb6f5bf9acb3058c79b5f826
mlambdasoo <mlambdasoo@gmail.com>
2024-10-09 14:41:16 +0900

    df

--- 2_SAC_CustomWidget/Sudong/EXEERCISE/BASIC/Base.js
+++ 2_SAC_CustomWidget/Sudong/EXEERCISE/BASIC/Base.js
@@ -1,25 +1,144 @@
 (function () {
-  const temp = document.createElement("template");
-  temp.innerHTML = `
-    <style>
-    </style>
-    <div id="root" style="width: 100%; height: 100%;">
-    </div>
- `;
-  class CustomWidget extends HTMLElement {
+  let version = "0.2.6";
+  let tmpl = document.createElement("template");
+  tmpl.innerHTML = ``;
+
+  class DatePicker extends HTMLElement {
     constructor() {
       super();
-      this._shadowRoot = this.attachShadow({ mode: "open" });
-      this._shadowRoot.appendChild(temp.content.cloneNode(true));
+      this.init();
     }
 
-    onCustomWidgetBeforeUpdate(changedProps) {}
+    init(skipChildrenCheck) {
+      console.log(skipChildrenCheck);
+      console.log(this.children);
+      // if (skipChildrenCheck !== true && this.children.length === 2) return; //constructor called during drag+drop
+      // if (!this.querySelector("link")) {
+      //   this.appendChild(tmpl.content.cloneNode(true));
+      // }
 
-    onCustomWidgetAfterUpdate(changedProps) {}
+      var dp = new sap.m.Button({
+        text: "Get Data",
+        press: function (oEvent) {
+          console.log("tt");
+        },
+      });
+      dp.placeAt(this);
 
-    onCustomWidgetDestroy() {}
+      // var ctor = sap.m.DatePicker;
+      // // if (this._enablerange) {
+      // //   ctor = sap.m.DateRangeSelection;
+      // // }
+      // this.DP = new ctor({
+      //   change: function () {
+      //     // this.fireChanged();
+      //     // this.dispatchEvent(new Event("onChange"));
+      //   }.bind(this),
+      // }).addStyleClass("datePicker");
+      // if (this._format) {
+      //   this.DP.setDisplayFormat(this._format);
+      // }
+      // if (this._minDate) {
+      //   this.updateMinDate();
+      // }
+      // if (this._maxDate) {
+      //   this.updateMaxDate();
+      // }
+      // this.DP.placeAt(this);
+    }
 
-    onCustomWidgetResize(width, height) {}
+    // fireChanged() {
+    //   var properties = { dateVal: this.DP.getDateValue() };
+    //   if (this._enablerange) {
+    //     properties.secondDateVal = this.DP.getSecondDateValue();
+    //   }
+    //   this.dispatchEvent(
+    //     new CustomEvent("propertiesChanged", {
+    //       detail: {
+    //         properties: properties,
+    //       },
+    //     })
+    //   );
+    // }
+
+    // clear() {
+    //   this.DP.setValue("");
+    // }
+
+    // getDateVal() {
+    //   return this.DP.getDateValue() || undefined;
+    // }
+
+    // set dateVal(value) {
+    //   if (value == undefined || !this.DP) return;
+    //   if (typeof value === "string") value = new Date(value);
+    //   this.DP.setDateValue(value);
+    // }
+
+    // set secondDateVal(value) {
+    //   if (value == undefined || !this.DP || !this._enablerange) return;
+    //   if (typeof value === "string") value = new Date(value);
+    //   this.DP.setSecondDateValue(value);
+    // }
+
+    // set format(value) {
+    //   if (!this.DP) return;
+    //   this._format = value;
+    //   this.DP.setDisplayFormat(value);
+    // }
+
+    // set darktheme(value) {
+    //   this.querySelector("link").setAttribute(
+    //     "href",
+    //     `https://widgets.nkappler.de/datepicker/releases/${version}/${
+    //       value ? "dark" : "light"
+    //     }.css`
+    //   );
+    // }
+
+    // set enablerange(value) {
+    //   if (value == undefined || !this.DP) return;
+    //   this._enablerange = value;
+    //   this.DP.destroy();
+    //   this.init(true);
+    // }
+
+    // set minDateVal(date) {
+    //   if (!this.DP) return;
+    //   this._minDate = date;
+    //   this.updateMinDate();
+    // }
+
+    // set maxDateVal(date) {
+    //   if (!this.DP) return;
+    //   this._maxDate = date;
+    //   this.updateMaxDate();
+    // }
+
+    // updateMaxDate() {
+    //   if (!!this._maxDate) {
+    //     if (this.DP.getDateValue() > this._maxDate) {
+    //       this.DP.setDateValue(this._maxDate);
+    //     }
+    //     if (this._enablerange && this.DP.getSecondDateValue() > this._maxDate) {
+    //       this.DP.setSecondDateValue(this._maxDate);
+    //     }
+    //   }
+    //   this.DP.setMaxDate(this._maxDate);
+    // }
+
+    // updateMinDate() {
+    //   if (!!this._maxDate) {
+    //     if (this.DP.getDateValue() < this._minDate) {
+    //       this.DP.setDateValue(this._minDate);
+    //     }
+    //     if (this._enablerange && this.DP.getSecondDateValue() < this._minDate) {
+    //       this.DP.setSecondDateValue(this._minDate);
+    //     }
+    //   }
+    //   this.DP.setMinDate(this._minDate);
+    // }
   }
-  customElements.define("com-sapkorea-sac-sudong-base", CustomWidget);
+
+  customElements.define("com-sapkorea-sac-sudong-base", DatePicker);
 })();
