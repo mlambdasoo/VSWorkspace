e1641c1386b765b90f18902de653a2efdebfe356
mlambdasoo <mlambdasoo@gmail.com>
2024-10-21 09:15:30 +0900

    df

--- 2_SAC_CustomWidget/Sudong/InputControl/UI5/Base.js
+++ 2_SAC_CustomWidget/Sudong/InputControl/UI5/Base.js
@@ -3,134 +3,43 @@
   let tmpl = document.createElement("template");
   tmpl.innerHTML = ``;
 
-  class DatePicker extends HTMLElement {
+  class InputControl extends HTMLElement {
     constructor() {
       super();
       this.init();
     }
 
-    init(skipChildrenCheck) {
-      console.log(skipChildrenCheck);
-      console.log(this.children);
-      if (skipChildrenCheck !== true && this.children.length === 2) return; //constructor called during drag+drop
-      if (!this.querySelector("link")) {
-        this.appendChild(tmpl.content.cloneNode(true));
-      }
-
-      var ctor = sap.m.DatePicker;
-      if (this._enablerange) {
-        ctor = sap.m.DateRangeSelection;
-      }
+    init() {
+      var oData = {
+        root: {
+          text: "Root",
+          nodes: [
+            {
+              text: "Node 1",
+              nodes: [{ text: "Subnode 1.1" }, { text: "Subnode 1.2" }],
+            },
+            {
+              text: "Node 2",
+              nodes: [{ text: "Subnode 2.1" }, { text: "Subnode 2.2" }],
+            },
+          ],
+        },
+      };
+      var oModel = new sap.ui.model.json.JSONModel(oData);
+      var ctor = sap.m.Tree;
       this.DP = new ctor({
-        change: function () {
-          this.fireChanged();
-          this.dispatchEvent(new Event("onChange"));
-        }.bind(this),
-      }).addStyleClass("datePicker");
-      if (this._format) {
-        this.DP.setDisplayFormat(this._format);
-      }
-      if (this._minDate) {
-        this.updateMinDate();
-      }
-      if (this._maxDate) {
-        this.updateMaxDate();
-      }
+        items: {
+          path: "/root/nodes",
+          template: new sap.m.StandardTreeItem({
+            title: "{text}",
+          }),
+        },
+      });
+      this.DP.setModel(oModel);
+
       this.DP.placeAt(this);
-    }
-
-    fireChanged() {
-      var properties = { dateVal: this.DP.getDateValue() };
-      if (this._enablerange) {
-        properties.secondDateVal = this.DP.getSecondDateValue();
-      }
-      this.dispatchEvent(
-        new CustomEvent("propertiesChanged", {
-          detail: {
-            properties: properties,
-          },
-        })
-      );
-    }
-
-    clear() {
-      this.DP.setValue("");
-    }
-
-    getDateVal() {
-      return this.DP.getDateValue() || undefined;
-    }
-
-    set dateVal(value) {
-      if (value == undefined || !this.DP) return;
-      if (typeof value === "string") value = new Date(value);
-      this.DP.setDateValue(value);
-    }
-
-    set secondDateVal(value) {
-      if (value == undefined || !this.DP || !this._enablerange) return;
-      if (typeof value === "string") value = new Date(value);
-      this.DP.setSecondDateValue(value);
-    }
-
-    set format(value) {
-      if (!this.DP) return;
-      this._format = value;
-      this.DP.setDisplayFormat(value);
-    }
-
-    set darktheme(value) {
-      this.querySelector("link").setAttribute(
-        "href",
-        `https://widgets.nkappler.de/datepicker/releases/${version}/${
-          value ? "dark" : "light"
-        }.css`
-      );
-    }
-
-    set enablerange(value) {
-      if (value == undefined || !this.DP) return;
-      this._enablerange = value;
-      this.DP.destroy();
-      this.init(true);
-    }
-
-    set minDateVal(date) {
-      if (!this.DP) return;
-      this._minDate = date;
-      this.updateMinDate();
-    }
-
-    set maxDateVal(date) {
-      if (!this.DP) return;
-      this._maxDate = date;
-      this.updateMaxDate();
-    }
-
-    updateMaxDate() {
-      if (!!this._maxDate) {
-        if (this.DP.getDateValue() > this._maxDate) {
-          this.DP.setDateValue(this._maxDate);
-        }
-        if (this._enablerange && this.DP.getSecondDateValue() > this._maxDate) {
-          this.DP.setSecondDateValue(this._maxDate);
-        }
-      }
-      this.DP.setMaxDate(this._maxDate);
-    }
-
-    updateMinDate() {
-      if (!!this._maxDate) {
-        if (this.DP.getDateValue() < this._minDate) {
-          this.DP.setDateValue(this._minDate);
-        }
-        if (this._enablerange && this.DP.getSecondDateValue() < this._minDate) {
-          this.DP.setSecondDateValue(this._minDate);
-        }
-      }
-      this.DP.setMinDate(this._minDate);
     }
   }
 
-  customElements.define("com-sapkorea-sac-sudong-base", DatePicker);
+  customElements.define("com-sapkorea-sac-sudong-base", InputControl);
 })();
