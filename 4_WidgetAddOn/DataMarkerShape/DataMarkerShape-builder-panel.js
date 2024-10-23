1eaaa0194ba639b1d0a29c72134c895f09a93da4
mlambdasoo <mlambdasoo@gmail.com>
2024-10-23 11:12:10 +0900

    .

--- 4_WidgetAddOn/DataMarkerShape/DataMarkerShape-builder-panel.js
+++ 4_WidgetAddOn/DataMarkerShape/DataMarkerShape-builder-panel.js
@@ -1,5 +1,6 @@
-const plotareaFormTemplate = document.createElement("template");
-plotareaFormTemplate.innerHTML = `
+(function () {
+  const plotareaFormTemplate = document.createElement("template");
+  plotareaFormTemplate.innerHTML = `
     <form id="form">
         <fieldset>
             <legend>Plotarea Properties</legend>
@@ -23,52 +24,55 @@ plotareaFormTemplate.innerHTML = `
     </style>
 `;
 
-class VizPlotareaBuilderPanel extends HTMLElement {
-  constructor() {
-    super();
-    this._shadowRoot = this.attachShadow({ mode: "open" });
-    this._shadowRoot.appendChild(plotareaFormTemplate.content.cloneNode(true));
-    this._shadowRoot
-      .getElementById("form")
-      .addEventListener("submit", this._submit.bind(this));
-    this._shadowRoot
-      .getElementById("shapeSelect")
-      .addEventListener("change", this._submit.bind(this));
-    this._shadowRoot
-      .getElementById("linecolor")
-      .addEventListener("change", this._submit.bind(this));
-  }
+  class VizPlotareaBuilderPanel extends HTMLElement {
+    constructor() {
+      super();
+      this._shadowRoot = this.attachShadow({ mode: "open" });
+      this._shadowRoot.appendChild(
+        plotareaFormTemplate.content.cloneNode(true)
+      );
+      this._shadowRoot
+        .getElementById("form")
+        .addEventListener("submit", this._submit.bind(this));
+      this._shadowRoot
+        .getElementById("shapeSelect")
+        .addEventListener("change", this._submit.bind(this));
+      this._shadowRoot
+        .getElementById("linecolor")
+        .addEventListener("change", this._submit.bind(this));
+    }
 
-  _submit(e) {
-    e.preventDefault();
-    this.dispatchEvent(
-      new CustomEvent("propertiesChanged", {
-        detail: {
-          properties: {
-            // property 변경--rounded
-            dataMarkerShape: this.dataMarkerShape,
-            lineColor: this.lineColor,
+    _submit(e) {
+      e.preventDefault();
+      this.dispatchEvent(
+        new CustomEvent("propertiesChanged", {
+          detail: {
+            properties: {
+              // property 변경--rounded
+              dataMarkerShape: this.dataMarkerShape,
+              lineColor: this.lineColor,
+            },
           },
-        },
-      })
-    );
+        })
+      );
+    }
+
+    set dataMarkerShape(value) {
+      this._shadowRoot.getElementById("shapeSelect").value = value;
+    }
+
+    get dataMarkerShape() {
+      return this._shadowRoot.getElementById("shapeSelect").value;
+    }
+
+    set lineColor(value) {
+      this._shadowRoot.getElementById("linecolor").value = value;
+    }
+
+    get lineColor() {
+      return this._shadowRoot.getElementById("linecolor").value;
+    }
   }
 
-  set dataMarkerShape(value) {
-    this._shadowRoot.getElementById("shapeSelect").value = value;
-  }
-
-  get dataMarkerShape() {
-    return this._shadowRoot.getElementById("shapeSelect").value;
-  }
-
-  set lineColor(value) {
-    this._shadowRoot.getElementById("linecolor").value = value;
-  }
-
-  get lineColor() {
-    return this._shadowRoot.getElementById("linecolor").value;
-  }
-}
-
-customElements.define("viz-plotarea-build", VizPlotareaBuilderPanel);
+  customElements.define("viz-plotarea-build", VizPlotareaBuilderPanel);
+})();
