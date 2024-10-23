10c19962d4102138ef90395b9dc9d013d0d088ab
mlambdasoo <mlambdasoo@gmail.com>
2024-10-23 10:13:54 +0900

    .

--- 4_WidgetAddOn/DataMarkerShape/DataMarkerShape.js
+++ 4_WidgetAddOn/DataMarkerShape/DataMarkerShape.js
@@ -89,6 +89,9 @@
         return;
       }
 
+      // 각 시리즈마다 새로운 점 배열 시작
+      const seriesPoints = [];
+
       singleSeries.dataPoints.forEach((dataPoint) => {
         const { dataInfo, labelInfo } = dataPoint;
 
@@ -96,14 +99,25 @@
           labelInfo.pointValue = parseInt(dataInfo.pointValue[0]);
         }
 
-        // Render the data marker for current data point
+        // 점 정보를 현재 시리즈의 배열에 추가
+        if (dataInfo && !dataInfo.hidden && !dataInfo.outOfViewport) {
+          seriesPoints.push({
+            x: dataInfo.x + dataInfo.width / 2,
+            y: dataInfo.y + dataInfo.height / 2,
+          });
+        }
+
         this.renderData(dataInfo, options);
 
-        // Render the data label for current data point
         if (labelInfo) {
           this.renderLabel(labelInfo, options);
         }
       });
+
+      // 현재 시리즈의 점들을 전체 points 배열에 추가
+      if (seriesPoints.length > 0) {
+        this._points.push(seriesPoints);
+      }
     }
 
     renderData(dataInfo, options) {
@@ -142,16 +156,10 @@
       );
 
       this._markersContainer.appendChild(dataElement);
-
-      // 점 위치 저장
-      this._points.push({ x: x + width / 2, y: y + height / 2 });
     }
 
     drawLinesBetweenPoints() {
       const ctx = this._canvasElement.getContext("2d");
-
-      if (this._points.length < 2) return;
-
       ctx.clearRect(
         0,
         0,
@@ -159,17 +167,26 @@
         this._canvasElement.height
       );
 
-      // 선 스타일 설정
-      ctx.strokeStyle = "black";
-      ctx.lineWidth = 2;
-      ctx.setLineDash([5, 5]);
+      // 각 시리즈별로 선 그리기
+      this._points.forEach((seriesPoints) => {
+        if (seriesPoints.length < 2) return;
 
-      ctx.beginPath();
-      ctx.moveTo(this._points[0].x, this._points[0].y);
-      for (let i = 1; i < this._points.length; i++) {
-        ctx.lineTo(this._points[i].x, this._points[i].y);
-      }
-      ctx.stroke();
+        // 선 스타일 설정
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 2;
+        ctx.setLineDash([5, 5]);
+
+        ctx.beginPath();
+        // 첫 번째 점으로 이동
+        ctx.moveTo(seriesPoints[0].x, seriesPoints[0].y);
+
+        // 나머지 점들을 순서대로 연결
+        for (let i = 1; i < seriesPoints.length; i++) {
+          ctx.lineTo(seriesPoints[i].x, seriesPoints[i].y);
+        }
+
+        ctx.stroke();
+      });
     }
 
     renderLabel(labelInfo, options) {
