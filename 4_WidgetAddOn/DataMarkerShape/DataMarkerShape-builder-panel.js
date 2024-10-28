fa9c047fefb7337297f4b8bae0e908349346521e
mlambdasoo <mlambdasoo@gmail.com>
2024-10-28 14:11:47 +0900

    d

--- 4_WidgetAddOn/DataMarkerShape/DataMarkerShape-builder-panel.js
+++ 4_WidgetAddOn/DataMarkerShape/DataMarkerShape-builder-panel.js
@@ -120,31 +120,40 @@
       );
     }
 
-    set measure0Name(value) {
+    set measure0Settings(value) {
+      console.log(value);
       this._shadowRoot.getElementById("measure0_Name").value = value;
     }
-    get measure0Name() {
+    get measure0Settings() {
+      console.log(this._shadowRoot.getElementById("measure0_Name").value);
       return this._shadowRoot.getElementById("measure0_Name").value;
     }
-    set measure0Dotted(value) {
-      this._shadowRoot.getElementById("measure0_Dotted").checked = !!value;
-    }
-    get measure0Dotted() {
-      return this._shadowRoot.getElementById("measure0_Dotted").checked;
-    }
-    set measure0LineColor(value) {
-      this._shadowRoot.getElementById("measure0_LineColor").value = value;
-    }
 
-    get measure0LineColor() {
-      return this._shadowRoot.getElementById("measure0_LineColor").value;
-    }
-    set measure0MarkerShape(value) {
-      this._shadowRoot.getElementById("measure0_shape").value = value;
-    }
-    get measure0MarkerShape() {
-      return this._shadowRoot.getElementById("measure0_shape").value;
-    }
+    // set measure0Name(value) {
+    //   this._shadowRoot.getElementById("measure0_Name").value = value;
+    // }
+    // get measure0Name() {
+    //   return this._shadowRoot.getElementById("measure0_Name").value;
+    // }
+    // set measure0Dotted(value) {
+    //   this._shadowRoot.getElementById("measure0_Dotted").checked = !!value;
+    // }
+    // get measure0Dotted() {
+    //   return this._shadowRoot.getElementById("measure0_Dotted").checked;
+    // }
+    // set measure0LineColor(value) {
+    //   this._shadowRoot.getElementById("measure0_LineColor").value = value;
+    // }
+
+    // get measure0LineColor() {
+    //   return this._shadowRoot.getElementById("measure0_LineColor").value;
+    // }
+    // set measure0MarkerShape(value) {
+    //   this._shadowRoot.getElementById("measure0_shape").value = value;
+    // }
+    // get measure0MarkerShape() {
+    //   return this._shadowRoot.getElementById("measure0_shape").value;
+    // }
   }
 
   customElements.define("viz-plotarea-build", VizPlotareaBuilderPanel);
